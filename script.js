c062f8c1abbc53d7466967d1ef7fed259a44b741
leencho <ahmed567moha@gmail.com>
2021-11-28 18:46:21 +0100

    js

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 
 var btn = document.getElementById("btn");
 var light = document.getElementById("lmp");
+var charecter = document.getElementById("block");
+
+
 
 function toggleBtn(){
 	btn.classList.toggle("active");
 	light.classList.toggle("on");
 
+
+}
+
+function jump()
+{
+		charecter.classList.toggle("animate");
 
 }
 
